da1ebad7096d12eb909829513b8d1b5ea64d5ab6
osama <osama.mn@live.com>
2018-03-03 22:11:54 +0200

    heroku optimized

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const hbs = require("hbs");
 const fs = require("fs");
 
+const port = process.env.PORT || 3000;
 var app = express();
 app.set("view engine", "hbs");
 
@@ -45,6 +46,6 @@ app.get("/about", (req, res) => {
     title: "about page"
   });
 });
-app.listen(3000, () => {
-  console.log("server is up on port 3000");
+app.listen(port, () => {
+  console.log("server is up on port " + port);
 });
